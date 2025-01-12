fb57763e2aec085b9d92ffcc7a390fb4cff28b15
animanamit <747animan@gmail.com>
2025-01-12 16:51:58 +0800

    configuring drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { env } from "@/env";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./db/schema.ts",
+  dialect: "postgresql",
+  out: "./drizzle",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+});
