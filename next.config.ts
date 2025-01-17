f878b0b255d90b5b3d7c997948efcf600f863583
Animan Amit <36138723+animanamit@users.noreply.github.com>
2025-01-17 22:59:33 +0800

    restart

--- next.config.ts
+++ next.config.ts
@@ -2,27 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": ".",
-    };
-    return config;
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "pxqmxxjuewlpptvibnhg.supabase.co",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
