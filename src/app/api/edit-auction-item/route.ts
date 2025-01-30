c2d48840f8f2024d774f03296535944f39c196b7
Animan Amit <36138723+animanamit@users.noreply.github.com>
2025-01-30 19:30:12 +0800

    fix build error

--- src/app/api/edit-auction-item/route.ts
+++ src/app/api/edit-auction-item/route.ts
@@ -1,10 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export async function PUT(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function PUT(request: Request, params: any) {
   const formData = await request.formData();
   const title = formData.get("title");
   const description = formData.get("description");
