d8ee681c0e51936278fffe779dc154b181a47a7d
animanamit <747animan@gmail.com>
2025-04-11 00:25:08 +0800

    compile changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,4 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect } from "react";
 import type { AppRouter } from "../../backend/index";
 //     👆 **type-only** import
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
@@ -9,7 +6,6 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client";
 
 // Pass AppRouter as generic here. 👇 This lets the `trpc` object know
 // what procedures are available on the server and their input/output types.
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
@@ -19,8 +15,6 @@ const trpc = createTRPCClient<AppRouter>({
 });
 
 function App() {
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
     (async () => {
       const users = await trpc.greeting.query();
@@ -29,28 +23,9 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-black h-full w-full flex justify-center items-center">
+      yuh
+    </div>
   );
 }
 
