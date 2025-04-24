4770d1184608ee13fd8c7fdf56791db2ab47aa52
Animan Amit <36138723+animanamit@users.noreply.github.com>
2025-04-24 14:44:35 +0800

    remove trpc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,8 @@
-import { useEffect } from "react";
-import type { AppRouter } from "../../backend/index";
-//     👆 **type-only** import
-import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { Button } from "@/components/ui/button";
 import { useNotification } from "@/contexts/notification-context";
 
-// Pass AppRouter as generic here. 👇 This lets the `trpc` object know
-// what procedures are available on the server and their input/output types.
-const trpc = createTRPCClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: "http://localhost:3000/trpc", // Updated endpoint
-    }),
-  ],
-});
-
 function App() {
   const { notify } = useNotification();
-
-  useEffect(() => {
-    (async () => {
-      const users = await trpc.greeting.query();
-      console.log(users);
-    })();
-  }, []);
 
   return (
     <div className="bg-black h-full w-full flex justify-center items-center">
