9cb2b81b4b33fc360dd495b325dd187725c9dd20
Animan Amit <36138723+animanamit@users.noreply.github.com>
2025-04-24 18:02:12 +0800

    remove trpc

--- frontend/src/trpc.ts
+++
@@ -1,4 +0,0 @@
-import { createTRPCContext } from "@trpc/tanstack-react-query";
-import type { AppRouter } from "../../backend/index";
-export const { TRPCProvider, useTRPC, useTRPCClient } =
-  createTRPCContext<AppRouter>();
